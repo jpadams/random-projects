e4bb312bf553905586812d4c3bcf74296ba8868d
jeremy--adams <jasprintww@yahoo.com>
2013-01-06 22:15:50 -0800

    Added comment to sorted_uids.rb regarding using cut shell command instead

--- sorted_uids.rb
+++ sorted_uids.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/ruby
-
+# could have done this in bash with
+# cut -d ':' -f  3 /etc/passwd | sort -n > foo.out
+#
+# but couldn't quite think of that during interview :)
+ 
 pw = File.open("/etc/passwd")
 uid = []
 pw.each { |line| uid.push(line.split(':')[2].to_i) }
